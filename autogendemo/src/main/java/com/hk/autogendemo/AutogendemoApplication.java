3e21c048e7294e32309160c3fe98ad9e32b1f4b1
kun.he <kun.he@tendcloud.com>
2018-04-11 19:15:13 +0800

    init commit

---
+++ autogendemo/src/main/java/com/hk/autogendemo/AutogendemoApplication.java
@@ -0,0 +1,12 @@
+package com.hk.autogendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutogendemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutogendemoApplication.class, args);
+	}
+}
